092806075c5815e0ff974548aa19ca9859ea6681
goodstealing <xxxvladoss@gmail.com>
2024-09-23 12:20:16 +0300

    Class Task3

---
+++ Module1/Task3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1
+{
+    internal class Task3
+    {
+        public static void Execute()
+        {
+            Console.Clear();
+            Console.CursorVisible = true;
+
+            Console.Write("Введите Имя: ");
+            string? name = Console.ReadLine();
+            Console.Write("Введите Фамилию: ");
+            string? surname = Console.ReadLine();
+
+            Console.WriteLine($"{surname},{name}"); // Вывод "Фамилия, Имя"
+        }
+    }
+}
