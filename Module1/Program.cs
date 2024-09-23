a56fac05e228c8dafe446f9a9e6aa10903eaf185
goodstealing <xxxvladoss@gmail.com>
2024-09-23 11:53:56 +0300

    Class handler & Class Task1

--- Module1/Program.cs
+++ Module1/Program.cs
@@ -1,2 +1,78 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Module1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            DisplayInstructions(); // Инструкции для пользователя
+
+            bool exit = false; // Переменная для контроля выхода из программы
+
+            while (!exit)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true); 
+                char key = keyInfo.KeyChar; 
+
+                if (char.IsDigit(key) && key >= '1' && key <= '9')
+                {
+                    ExecuteTask(key);
+                }
+                else if (key == (char)ConsoleKey.Escape)
+                {
+                    exit = true;
+                }
+                else
+                {
+                    Console.WriteLine("/Ожидаются цифры от 1 до 9 или 0/"); 
+                }
+            }
+        }
+
+        /// <summary>
+        /// Выводит инструкции по использованию программы.
+        /// </summary>
+        static void DisplayInstructions()
+        {
+            Console.WriteLine("| Система Запуска Заданий | Вариант 2 |");
+            Console.WriteLine("| Для продолжения выберите номер задания и нажмите соответствующую клавишу |");
+            Console.WriteLine("| Пример: клавиша 1 - Задание 1 | Для выхода 'Esc' |\n");
+        }
+
+        /// <summary>
+        /// Выполняет запуск задания по номеру, основанному на нажатой клавише.
+        /// </summary>
+        /// <param name="key">Символ, представляющий номер задания.</param>
+        /// <remarks>
+        /// Использует рефлексию для поиска и вызова метода Execute() класса,
+        /// соответствующего номеру задания. Выводит сообщения об ошибках, если
+        /// класс или метод не найдены.
+        /// </remarks>
+        static void ExecuteTask(char key)
+        {
+            int taskNumber = key - '0'; // Символ => целое число
+            string className = $"Task{taskNumber}"; // Формирование имени класса
+
+            // Рефлексия для поиска и вызова метода Execute
+            Type ?taskType = Type.GetType($"Module1.{className}");
+            if (taskType != null)
+            {
+                var method = taskType.GetMethod("Execute");
+                if (method != null)
+                {
+                    method.Invoke(null, null); // Вызов метода Execute
+                }
+                else
+                {
+                    Console.WriteLine($"Метод Execute() не найден в классе '{className}'.");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"| Запуск: Класс {className} не найден, запуск невозможен.");
+            }
+        }
+    }
+}
---
+++ Module1/Task1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Module1
+{
+    internal class Task1
+    {
+        // Nullable строка для хранения ввода пользователя
+        public static string? userinput;
+
+        /// <summary>
+        /// Определение четного или нечетного числа.
+        /// </summary>
+        /// <returns>Вывод четного или нечетного числа на консоль.</returns>
+        public static void Execute()
+        {
+
+            Console.Clear();
+            Console.CursorVisible = true;
+
+            Console.Write("Введите целое число: ");
+            string? userinput = Console.ReadLine();
+
+            // Проверка введённой строки на соответствие целому числу
+            if (int.TryParse(userinput, out _))
+            {
+                int number = int.Parse(userinput);
+                Console.WriteLine($"Число {number} соответствует требованиям.");
+
+                // Проверка чётное|нечётное
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine($"Число: {number} чётное."); 
+                }
+                else
+                {
+                    Console.WriteLine($"Число: {number} нечётное."); 
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: введено не целое число.");
+            }
+            Console.WriteLine("\n| Запустить другое задание? | Выход 'Esc' |");
+        }
+    }
+}
