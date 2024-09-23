9cb6a0c2596e274d8b34afb96c178a400c3a407d
goodstealing <xxxvladoss@gmail.com>
2024-09-23 12:11:24 +0300

    Class Task2

---
+++ Module1/Task2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Module1
+{
+    internal class Task2
+    {
+        public static void Execute()
+        {
+            Console.Clear();
+            Console.CursorVisible = true;
+
+            Console.Write("Введите радиус круга: ");
+            string? userinput = Console.ReadLine();
+            double radius;
+
+            // Попытка преобразовать ввод в действительное число
+            if (double.TryParse(userinput, out radius))
+            {
+                Console.WriteLine($"Вы ввели число: {radius}");
+                var s = Math.PI * radius; // Формула площади круга по радиусу
+
+                Console.WriteLine("Площадь круга: ");
+            }
+            else
+            {
+                Console.WriteLine("/Введено некорректное значение, введите действительное число./");
+            }
+        }
+    }
+}
