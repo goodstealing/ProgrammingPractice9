2703e0d7fb9a69e12af04f13fe0233d5f8fbb33d
goodstealing <xxxvladoss@gmail.com>
2024-10-07 13:46:53 +0300

    Null Tasks Init

--- src/Module3/Task1.xaml.cs
+++ src/Module3/Task1.xaml.cs
@@ -1,22 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Module3.AdditionalClasses;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
-using System.Windows.Input;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
 
 namespace Module3
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class Task1 : Window
     {
-        public MainWindow()
+        public Task1()
         {
             InitializeComponent();
         }
+
+
+
+
+
 
         void CloseButton_Click(object sender, RoutedEventArgs e) => Close();
 
@@ -80,6 +82,14 @@ namespace Module3
             Task5 task5 = new();
             task5.Left = Left; task5.Top = Top;
             task5.Show();
+            Hide();
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new();
+            mainWindow.Show();
+            mainWindow.Left = Left; mainWindow.Top = Top;
             Hide();
         }
     }
--- src/Module3/Task4.xaml.cs
+++ src/Module3/Task4.xaml.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Input;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 
 namespace Module3
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для Task4.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Task4 : Window
     {
-        public MainWindow()
+        public Task4()
         {
             InitializeComponent();
         }
@@ -80,6 +86,14 @@ namespace Module3
             Task5 task5 = new();
             task5.Left = Left; task5.Top = Top;
             task5.Show();
+            Hide();
+        }
+
+        private void ExitButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new();
+            mainWindow.Show();
+            mainWindow.Left = Left; mainWindow.Top = Top;
             Hide();
         }
     }
